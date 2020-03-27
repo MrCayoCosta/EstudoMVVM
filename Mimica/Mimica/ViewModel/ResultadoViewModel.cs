312e63bcb74ac36a64a914a6aedf41188d7d0f6f
Cayo Costa <cayosantos@gmail.com>
2020-03-27 12:37:06 -0300

    Tela de resultado finalizada

--- Mimica/Mimica/ViewModel/ResultadoViewModel.cs
+++ Mimica/Mimica/ViewModel/ResultadoViewModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Mimica.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace Mimica.ViewModel
 {
     public class ResultadoViewModel : INotifyPropertyChanged
     {
+        public Jogo Jogo { get; set; }
+        public Command JogarNovamente { get; set; }
+
+        public ResultadoViewModel()
+        {
+            Jogo = Armazanemanto.Armazenamento.Jogo;
+            JogarNovamente = new Command(JogarNovamenteAction);
+        }
+
+        private void JogarNovamenteAction()
+        {
+            App.Current.MainPage = new View.Inicio();
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void OnPropertyChanged(string NameProperty)
